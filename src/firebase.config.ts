a530b617a080f3123a74ad30ef8f493535592aab
Thomas Briand <tom.briiand@gmail.com>
2018-07-30 10:57:39 +0200

    Move env var into .env file, remove .config.ts files from gitignore

---
+++ src/firebase.config.ts
@@ -0,0 +1,9 @@
+// Configure Firebase.
+export default {
+	apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID
+};
